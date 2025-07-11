b4823af16a9e175786c503700571f7cc20f805a4
Guilherme Bonaldi <guilhermeguimaraesbonaldi031@gmail.com>
2025-07-11 11:18:23 -0300

    JS inicio

---
+++ script.js
@@ -0,0 +1,11 @@
+const apiKeyinput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askBotton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('form')
+
+const enviarFormulario (event) => {
+
+}
+form.addEventListener('submit', enviarFormulario)
